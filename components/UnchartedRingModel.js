b48c2dd18068fb011558bc9ef75718f1a28a0000
Vinh Pham <pham.trong.vinh@pwc.com>
2022-03-24 14:12:12 +0700

    simplify

--- components/UnchartedRingModel.js
+++ components/UnchartedRingModel.js
@@ -1,10 +1,10 @@
 import React, { useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
-import { Environment, useGLTF } from '@react-three/drei'
+// import { Environment, useGLTF } from '@react-three/drei'
 
 export default function Model({ ...props }) {
   const group = useRef()
-  const { nodes, materials } = useGLTF('/static/images/uncharted-ring-draco-transformed.glb')
+  // const { nodes, materials } = useGLTF('/static/images/uncharted-ring-draco-transformed.glb')
 
   useFrame(({ clock }) => {
     group.current.rotation.y = clock.getElapsedTime() * 0.2
@@ -12,11 +12,11 @@ export default function Model({ ...props }) {
 
   return (
     <group ref={group} {...props} dispose={null}>
-      <Environment files="/static/images/studio_small_03_1k.hdr" />
+      {/* <Environment files="/static/images/studio_small_03_1k.hdr" /> */}
       <mesh
         position={[0, -0.6, 0]}
-        geometry={nodes.Mesh_0.geometry}
-        material={materials['Material.001']}
+        // geometry={nodes.Mesh_0.geometry}
+        // material={materials['Material.001']}
         rotation={[-1.61, 0, -Math.PI]}
         scale={0.13}
       />
@@ -24,4 +24,4 @@ export default function Model({ ...props }) {
   )
 }
 
-useGLTF.preload('/static/images/uncharted-ring-draco-transformed.glb')
+// useGLTF.preload('/static/images/uncharted-ring-draco-transformed.glb')
