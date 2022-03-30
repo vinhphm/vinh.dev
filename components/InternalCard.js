536ed83a992f07d82c6f90d47f58e40d0bbd4b1f
Vinh Pham <vinhpm@outlook.com>
2022-03-30 23:19:13 +0700

    prepare cards

---
+++ components/InternalCard.js
@@ -0,0 +1,16 @@
+import Link from '@/components/Link'
+
+export default function InternalCard({ href, title, children }) {
+  return (
+    <Link
+      href={href}
+      title={title}
+      className=" w-full flex flex-col justify-between row-span-1 p-4 border-2 border-solid rounded-lg border-gray-200 dark:border-gray-800 transform hover:border-primary-600 dark:hover:border-primary-400"
+    >
+      <h3 className="text-lg font-bold leading-4 tracking-tight">{title}</h3>
+      <div className="pt-4 text-sm prose text-gray-800 max-w-none dark:text-gray-200 flex items-center gap-1">
+        {children}
+      </div>
+    </Link>
+  )
+}
