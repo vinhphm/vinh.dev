fe4e93c2acacd8fa9c0298e7ec35a94608d4a94e
Vinh Pham <vinhpm@outlook.com>
2022-04-03 11:45:15 +0700

    update Blog order

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/notes', title: 'Notes' },
   { href: '/blog', title: 'Blog' },
+  { href: '/notes', title: 'Notes' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
 ]
