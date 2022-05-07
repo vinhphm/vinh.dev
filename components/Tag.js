3a890f288d4dd4ba994959314c6b85ef10b89bab
Vinh Pham <vinh@vinh.dev>
2022-05-07 14:47:35 +0700

    tag component improve

--- components/Tag.js
+++ components/Tag.js
@@ -4,7 +4,10 @@ import kebabCase from '@/lib/utils/kebabCase'
 const Tag = ({ text }) => {
   return (
     <Link href={`/tags/${kebabCase(text)}`}>
-      <a className="mr-3 px-1 py-0.5 border border-neutral-100 dark:border-neutral-800 rounded-lg text-sm font-medium text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+      <a className="mr-3 px-1 py-0.5 border border-neutral-100 dark:border-neutral-900 rounded-lg text-sm">
+        <span className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+          #
+        </span>
         {text.split(' ').join('-')}
       </a>
     </Link>
