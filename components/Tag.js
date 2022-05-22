ab7093b1ae84d71a09ee80da4b49d4874d3ca322
Vinh Pham <vinh@vinh.dev>
2022-05-22 13:12:28 +0700

    revert tag design

--- components/Tag.js
+++ components/Tag.js
@@ -4,7 +4,7 @@ import kebabCase from '@/lib/utils/kebabCase'
 const Tag = ({ text }) => {
   return (
     <Link href={`/tags/${kebabCase(text)}`}>
-      <a className="mr-3 px-1 py-0.5 border border-neutral-100 dark:border-neutral-900 rounded-lg text-sm">
+      <a className="mr-3 text-sm">
         <span className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
           #
         </span>
