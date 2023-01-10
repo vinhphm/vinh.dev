375a064a34764dcb2c31c83cdf1f98e14a43a97e
Vinh Pham <vinh@vinh.dev>
2023-01-10 10:33:05 +0000

    wording update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -285,7 +285,7 @@ export default function Home({ articles }) {
         </title>
         <meta
           name="description"
-          content="I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms."
+          content="I’m Vinh, a software developer based in Ho Chi Minh City."
         />
       </Head>
       <Container className="mt-9">
