3f1b2c87101b88ee8e6d59d828f873c771436dfa
Vinh Pham <vinh@vinh.dev>
2023-02-18 23:41:31 +0700

    update css

--- src/components/MusicEqualizer.jsx
+++ src/components/MusicEqualizer.jsx
@@ -1,6 +1,8 @@
-export default function MusicEqualizer() {
+import clsx from 'clsx'
+
+export default function MusicEqualizer({ className }) {
   return (
-    <div className="flex h-3 w-4 items-center gap-0.75">
+    <div className={clsx(className, 'flex h-3 w-4 items-center gap-0.75')}>
       <span className="h-3 w-0.75 animate-shrink rounded-sm bg-spotify-green" />
       <span className="h-1.5 w-0.75 animate-expand rounded-sm bg-spotify-green" />
       <span className="h-3 w-0.75 animate-shrink rounded-sm bg-spotify-green" />
