00a4a74df76d0913e7eb8bb50345f0b56c7f935e
Vinh Pham <vinh@vinh.dev>
2023-03-02 01:17:53 +0700

    reduce image size

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,11 +14,11 @@ import {
   GitHubIcon,
   LinkedInIcon,
 } from 'components/SocialIcons'
-import image1 from 'images/photos/image-1.jpg'
-import image2 from 'images/photos/image-2.jpg'
-import image3 from 'images/photos/image-3.jpg'
-import image4 from 'images/photos/image-4.jpg'
-import image5 from 'images/photos/image-5.jpg'
+import cableCabin from 'images/photos/cable-cabin.jpg'
+import planeWindow from 'images/photos/plane-window.jpg'
+import macbookPro from 'images/photos/macbook-Pro.jpg'
+import phuquocSunset from 'images/photos/phuquoc-sunset.jpg'
+import coffeeShop from 'images/photos/coffee-shop.jpg'
 import logoPwC from 'images/logos/pwc.svg'
 import logoAccenture from 'images/logos/accenture.svg'
 import logoHitachi from 'images/logos/hitachi.svg'
@@ -259,23 +259,25 @@ function Photos() {
   return (
     <div className="mt-16 sm:mt-20">
       <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-        {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
-          <div
-            key={image.src}
-            className={clsx(
-              'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl',
-              rotations[imageIndex % rotations.length]
-            )}
-          >
-            <Image
-              src={image}
-              alt="Featured photo"
-              placeholder="blur"
-              sizes="(min-width: 640px) 18rem, 11rem"
-              className="absolute inset-0 h-full w-full object-cover"
-            />
-          </div>
-        ))}
+        {[cableCabin, planeWindow, macbookPro, phuquocSunset, coffeeShop].map(
+          (image, imageIndex) => (
+            <div
+              key={image.src}
+              className={clsx(
+                'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl',
+                rotations[imageIndex % rotations.length]
+              )}
+            >
+              <Image
+                src={image}
+                alt="Featured photo"
+                placeholder="blur"
+                sizes="(min-width: 640px) 18rem, 11rem"
+                className="absolute inset-0 h-full w-full object-cover"
+              />
+            </div>
+          )
+        )}
       </div>
     </div>
   )
