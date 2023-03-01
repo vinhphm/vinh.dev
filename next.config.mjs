88b08e7703607a485b17815ed479cff3abc39507
Vinh Pham <vinh@vinh.dev>
2023-03-01 09:18:59 +0000

    not pass tests

--- next.config.mjs
+++ next.config.mjs
@@ -13,17 +13,6 @@ const nextConfig = {
     newNextLinkBehavior: true,
     scrollRestoration: true,
   },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      })
-    }
-    return config
-  },
 }
 
 const withMDX = nextMDX({
