d79fbb14fe304ec061d3bfc933ca4f20c20b2638
Vinh Pham <vinh@vinh.dev>
2023-03-02 00:16:59 +0700

    add placeholder FCP

--- pages/index.jsx
+++ pages/index.jsx
@@ -270,6 +270,7 @@ function Photos() {
             <Image
               src={image}
               alt="Featured photo"
+              placeholder="blur"
               sizes="(min-width: 640px) 18rem, 11rem"
               className="absolute inset-0 h-full w-full object-cover"
               priority
