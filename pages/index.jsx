34602fc66882d2f8c5d87f79a1d8796686cae405
Vinh Pham <vinh@vinh.dev>
2023-03-02 03:30:26 +0000

    edit page

--- pages/index.jsx
+++ pages/index.jsx
@@ -72,19 +72,6 @@ function BriefcaseIcon(props) {
   )
 }
 
-function ArrowDownIcon(props) {
-  return (
-    <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" {...props}>
-      <path
-        d="M4.75 8.75 8 12.25m0 0 3.25-3.5M8 12.25v-8.5"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  )
-}
-
 function Article({ article }) {
   return (
     <Card as="article">
@@ -243,12 +230,13 @@ function Resume() {
           </li>
         ))}
       </ol>
-      {/*
-      <Button href="#" variant="secondary" className="group mt-6 w-full">
-        Download CV
-        <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
+      <Button
+        href="https://linkedin.com/in/vinhphm"
+        variant="secondary"
+        className="group mt-6 w-full"
+      >
+        More on LinkedIn
       </Button>
-      */}
     </div>
   )
 }
