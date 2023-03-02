e3c1909960530efb7323773b2a71da85a7281205
Vinh Pham <vinh@vinh.dev>
2023-03-02 04:21:30 +0000

    test speed next image

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,6 @@ import rehypePrism from '@mapbox/rehype-prism'
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx'],
   reactStrictMode: true,
-  images: {
-    formats: ['image/avif', 'image/webp'],
-  },
   experimental: {
     newNextLinkBehavior: true,
     scrollRestoration: true,
