482482fa2dbdcc21c272d9f1b45c1f5a7317bcab
Vinh Pham <vinh@vinh.dev>
2023-03-06 04:24:31 +0000

    migrating to astro

---
+++ src/lib/getArticles.ts
@@ -0,0 +1,7 @@
+import { getCollection } from "astro:content"
+
+export async function getArticles() {
+  const articles = await getCollection("articles")
+
+  return articles.sort((a, z) => z.data.date.getTime() - a.data.date.getTime())
+}
