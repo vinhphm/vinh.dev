0b3ad9e64ebad32723d193d1eac003c25979315f
Vinh Pham <vinh@vinh.dev>
2023-03-09 01:04:44 +0700

    revert

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import mdx from '@astrojs/mdx'
 import preact from '@astrojs/preact'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/edge'
+import vercel from '@astrojs/vercel/serverless'
 import { defineConfig } from 'astro/config'
 import rehypeExternalLinks from 'rehype-external-links'
 
