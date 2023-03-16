2000670dbecddb5d756e3b137dd4471cde1028d3
Vinh Pham <vinh@vinh.dev>
2023-03-16 03:29:25 +0000

    use wrong operator

--- src/lib/getArticles.ts
+++ src/lib/getArticles.ts
@@ -4,6 +4,6 @@ export async function getArticles() {
   const articles = await getCollection('articles')
 
   return articles
-    .filter(({ data }) => import.meta.env.DEV ?? !data.draft)
+    .filter(({ data }) => import.meta.env.DEV || !data.draft)
     .sort((a, z) => z.data.date.getTime() - a.data.date.getTime())
 }
