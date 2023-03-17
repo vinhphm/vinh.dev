17c2462efc64f6614a4f22fa7a2669da2a8d0503
Vinh Pham <vinh@vinh.dev>
2023-03-17 18:49:23 +0700

    fix image

--- src/pages/api/og.png.ts
+++ src/pages/api/og.png.ts
@@ -76,8 +76,8 @@ export const get: APIRoute = async ({ url, site }) => {
         >
           <img
             alt="Vinh.Dev"
-            height=${200}
-            width=${200}
+            height="{200}"
+            width="{200}"
             src="data:image/svg+xml,%3Csvg width='100%25' height='100%25' viewBox='0 0 256 256' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' xml:space='preserve' xmlns:serif='http://www.serif.com/' style='fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;'%3E%3Cg id='background'%3E%3Crect x='0' y='0' style='fill:white' width='256' height='256'/%3E%3C/g%3E%3Cg transform='matrix(0.629266,0,0,0.629266,55.9585,48.4261)'%3E%3Cg transform='matrix(1,0,0,1,-113.5,-79)'%3E%3Ccircle cx='324.29' cy='120.62' r='33.62' style='fill:black;stroke:black;stroke-width:7.95px;'/%3E%3C/g%3E%3Cg transform='matrix(1,0,0,1,-113.5,-79)'%3E%3Cpath d='M176.06,95.71C188.707,95.77 201.353,95.783 214,95.75C214.274,95.75 214.5,95.976 214.5,96.25L214.5,102.77C214.5,103.158 214.187,103.479 213.8,103.49C203.973,103.737 198.157,107.657 196.35,115.25C195.477,118.917 195.66,123.393 196.9,128.68C197.9,132.933 199.413,137.227 201.44,141.56C222.673,187.047 243.947,232.463 265.26,277.81C265.426,278.181 265.437,278.6 265.29,278.97L240.37,342.47C240.255,342.759 239.986,342.953 239.69,342.96L233.77,342.98C233.18,342.985 232.635,342.632 232.38,342.08C204.093,281.007 175.873,220.237 147.72,159.77C142.653,148.897 137.497,138.067 132.25,127.28C130.19,123.04 127.653,119.163 124.64,115.65C121.22,111.663 117.67,108.777 113.99,106.99C109.597,104.863 104.647,103.78 99.14,103.74C98.542,103.734 98.055,103.269 98.06,102.71L98.14,96.19C98.14,95.914 98.382,95.685 98.68,95.68C124.473,95.58 150.267,95.59 176.06,95.71Z' style='fill:black;fill-rule:nonzero;stroke:black;stroke-width:7.95px;'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E"
             style=${{ margin: '0 30px' }}
           />
