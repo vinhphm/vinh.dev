69042c450951bc26e55fa7af27e420f3f829860f
Vinh Pham <vinh@vinh.dev>
2024-04-12 03:04:38 +0000

    refine site

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -8,16 +8,19 @@
   >
     <svg viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
       <title>Vinh Pham @ vinh.dev</title>
-      <path class="cls-1" d="M31.4 47.4V53H25.8V41.8H20.2V25H14.6V13.8H25.8V25H31.4V41.8H37V47.4H31.4ZM48.2 25H42.6V13.8H53.8V19.4H48.2V25ZM42.6 30.6V36.2H37V30.6H42.6Z" />
+      <path
+        class="fill-color"
+        d="M31.4 47.4V53H25.8V41.8H20.2V25H14.6V13.8H25.8V25H31.4V41.8H37V47.4H31.4ZM48.2 25H42.6V13.8H53.8V19.4H48.2V25ZM42.6 30.6V36.2H37V30.6H42.6Z"
+      />
     </svg>
   </GlitchedElement>
 </template>
 
 <style scoped>
-.cls-1 {
+.fill-color {
   fill: #303030;
 }
-.dark .cls-1 {
+.dark .fill-color {
   fill: #fdfdfd;
 }
 </style>
