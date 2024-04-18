ef783658ddc630a4ac4986bb343bef9df3fe36e8
Vinh Pham <vinh@vinh.dev>
2024-04-18 16:57:26 +0700

    update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,15 @@
+<script setup lang="ts">
+import dayjs from 'dayjs'
+</script>
+
 <template>
   <div class="mt-10 mb-6 prose fw-inherit m-auto flex slide-enter animate-delay-1200!">
-    <span class="text-sm op75"><a target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/" style="color:inherit">CC BY-NC-SA 4.0</a> 2022-PRESENT © Vinh Pham</span>
+    <span class="text-sm op75">
+      © 2021-{{ dayjs().year() }} Vinh Pham.
+      <a target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/" style="color:inherit">
+        CC BY-NC-SA 4.0
+      </a>
+    </span>
     <div class="flex-auto" />
   </div>
 </template>
