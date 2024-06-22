879b8cd83f2f607d33f73cafefd8266c91020049
Vinh Pham <vinh@vinh.dev>
2024-06-22 17:06:37 +0700

    add back preload

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,7 @@ import sitemap from '@astrojs/sitemap'
 import UnoCSS from 'unocss/astro'
 import vue from '@astrojs/vue'
 import svgLoader from 'vite-svg-loader'
+import copy from 'rollup-plugin-copy'
 
 export default defineConfig({
   site: 'https://vinh.dev/',
@@ -23,7 +24,14 @@ export default defineConfig({
       svgLoader({
         svgo: false,
         defaultImport: 'url',
-      })
+      }),
+      copy({
+        targets: [
+          { src: 'node_modules/geist/dist/fonts/geist-sans/Geist-Variable.woff2', dest: 'dist/fonts/geist-sans/' },
+          { src: 'node_modules/geist/dist/fonts/geist-mono/GeistMono-Variable.woff2', dest: 'dist/fonts/geist-mono/' },
+        ],
+        hook: 'buildStart',
+      }),
     ]
   },
   markdown: {
