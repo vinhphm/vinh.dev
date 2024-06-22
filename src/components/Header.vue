66da0e57356959615e45fe4cfa8c46594d8d6a6e
Vinh Pham <vinh@vinh.dev>
2024-06-22 20:58:04 +0700

    change navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -58,21 +58,6 @@ onMounted(() => {
     }
   })
 })
-
-function toggleNavDrawer() {
-  const drawer = document.querySelector('.nav-drawer') as HTMLElement
-  const mask = document.querySelector('.nav-drawer-mask') as HTMLElement
-  if (!drawer || !mask)
-    return
-  if (drawer.style.transform === `translateX(0%)`) {
-    drawer.style.transform = `translateX(-100%)`
-    mask.style.display = `none`
-  }
-  else {
-    drawer.style.transform = `translateX(0%)`
-    mask.style.display = `block`
-  }
-}
 </script>
 
 <template>
@@ -81,10 +66,12 @@ function toggleNavDrawer() {
     class="!fixed bg-transparent z-899 w-screen h-20 px-6 flex justify-between items-center relative"
   >
     <div class="flex items-center h-full">
-      <a href="/" mr-6 aria-label="Header Logo Image">
+      <a href="/" aria-label="Logo">
         <Logo />
       </a>
-      <nav class="sm:flex hidden tracking-tighter flex-wrap gap-x-6 position-initial flex-row">
+    </div>
+    <div class="flex gap-x-6 items-center">
+      <nav class="flex tracking-tighter flex-wrap gap-x-6 position-initial flex-row">
         <a
           v-for="link in navLinks" :key="link.text" :aria-label="`${link.text}`" :target="getLinkTarget(link.href)"
           nav-link :href="link.href"
@@ -92,32 +79,14 @@ function toggleNavDrawer() {
           {{ link.text }}
         </a>
       </nav>
-      <div sm:hidden h-full flex items-center @click="toggleNavDrawer()">
-        <menu i-ri-menu-2-fill />
-      </div>
-    </div>
-    <div class="flex gap-x-6">
       <a
         v-for="link in socialLinks" :key="link.text" :aria-label="`${link.text}`" :class="link.icon" nav-link
-        :target="getLinkTarget(link.href)" :href="link.href"
+        :target="getLinkTarget(link.href)" :href="link.href" class="lt-md:hidden"
       />
-
-      <a nav-link target="_blank" href="/rss.xml" i-ri-rss-line aria-label="RSS" />
+      <a nav-link target="_blank" href="/rss.xml" class="lt-md:hidden" i-ri-rss-line aria-label="RSS" />
       <ThemeToggle />
     </div>
   </header>
-  <nav
-    class="nav-drawer sm:hidden"
-  >
-    <i i-ri-menu-2-fill />
-    <a
-      v-for="link in navLinks" :key="link.text" :aria-label="`${link.text}`" :target="getLinkTarget(link.href)"
-      nav-link :href="link.href" @click="toggleNavDrawer()"
-    >
-      {{ link.text }}
-    </a>
-  </nav>
-  <div class="nav-drawer-mask" @click="toggleNavDrawer()" />
 </template>
 
 <style scoped>
@@ -128,24 +97,5 @@ function toggleNavDrawer() {
 
 .header-bg-blur {
   --at-apply: backdrop-blur-md;
-}
-
-.nav-drawer {
-  transform: translateX(-100%);
-  --at-apply: box-border fixed h-screen z-999 left-0 top-0 min-w-32vw max-w-50vw
-    bg-main p-6 text-lg flex flex-col gap-5 transition-all;
-}
-
-.nav-drawer-mask {
-  display: none;
-  --at-apply: transition-all;
-  content: '';
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  background: rgba(0, 0, 0, 0.5);
-  z-index: 998;
 }
 </style>
