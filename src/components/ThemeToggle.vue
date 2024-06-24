9e7596894b219758e7431c3c7b02b6409a0c7921
Vinh Pham <vinh@vinh.dev>
2024-06-24 20:02:40 +0700

    reimplement theme toggle animation

--- src/components/ThemeToggle.vue
+++ src/components/ThemeToggle.vue
@@ -1,11 +1,62 @@
 <script lang="ts" setup>
-import { useDark, useToggle } from '@vueuse/core'
+import { nextTick } from 'vue'
+import { useDark } from '@vueuse/core'
 
 const isDark = useDark()
 
-const toggleDark = useToggle(isDark)
+function toggleDark(event: MouseEvent) {
+  // @ts-expect-error experimental API
+  const isAppearanceTransition = document.startViewTransition
+    && !window.matchMedia('(prefers-reduced-motion: reduce)').matches
+
+  if (!isAppearanceTransition) {
+    isDark.value = !isDark.value
+    return
+  }
+
+  // Add the theme-transition class to the root element to avoid conflicts
+  document.documentElement.classList.add('theme-transition')
+
+  const x = event.clientX
+  const y = event.clientY
+  const endRadius = Math.hypot(
+    Math.max(x, innerWidth - x),
+    Math.max(y, innerHeight - y),
+  )
+  // @ts-expect-error: Transition API
+  const transition = document.startViewTransition(async () => {
+    isDark.value = !isDark.value
+    await nextTick()
+  })
+
+  transition.ready.then(() => {
+    const clipPath = [
+      `circle(0px at ${x}px ${y}px)`,
+      `circle(${endRadius}px at ${x}px ${y}px)`,
+    ]
+    document.documentElement.animate(
+      {
+        clipPath: isDark.value
+          ? [...clipPath].reverse()
+          : clipPath,
+      },
+      {
+        duration: 400,
+        easing: 'ease-out',
+        pseudoElement: isDark.value
+          ? '::view-transition-old(root)'
+          : '::view-transition-new(root)',
+      },
+    )
+  }).finally(() => {
+    // Remove the class precisely after the transition
+    transition.finished.then(() => {
+      document.documentElement.classList.remove('theme-transition')
+    })
+  })
+}
 </script>
 
 <template>
-  <button :aria-label="isDark ? 'Dark Theme' : 'Light Theme'" nav-link dark:i-ri-moon-line i-ri-sun-line @click="toggleDark()" />
+  <button :aria-label="isDark ? 'Dark Theme' : 'Light Theme'" nav-link dark:i-ri-moon-line i-ri-sun-line @click="toggleDark" />
 </template>
