37301b63188569aa45d1f8d184dab670fa1151e6
Vinh Pham <vinh@vinh.dev>
2024-06-24 09:06:53 +0000

    fix external link

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,7 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import UnoCSS from 'unocss/astro'
 import vue from '@astrojs/vue'
+import rehypeExternalLinks from 'rehype-external-links'
 
 export default defineConfig({
   site: 'https://vinh.dev',
@@ -23,5 +24,8 @@ export default defineConfig({
       },
       wrap: true,
     },
+    rehypePlugins: [
+      [rehypeExternalLinks, { target: '_blank', rel: 'noopener' }],
+    ],
   },
 })
