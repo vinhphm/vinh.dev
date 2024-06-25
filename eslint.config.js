9d392f368bdaa5cdaa3b25c867254151b8ece90c
Vinh Pham <vinh@vinh.dev>
2024-06-25 10:46:06 +0000

    add smooth scroll for TOC

--- eslint.config.js
+++ eslint.config.js
@@ -12,6 +12,7 @@ export default antfu(
   },
   {
     rules: {
+      'format/prettier': 'off',
       'no-labels': 'off',
       'no-lone-blocks': 'off',
       'no-restricted-syntax': 'off',
@@ -20,6 +21,7 @@ export default antfu(
       'prefer-rest-params': 'off',
       'symbol-description': 'off',
       'style/operator-linebreak': 'off',
+      'style/brace-style': 'off',
       'ts/ban-types': 'off',
       'ts/no-invalid-this': 'off',
       'ts/no-unnecessary-type-constraint': 'off',
