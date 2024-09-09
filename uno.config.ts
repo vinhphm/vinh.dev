98ca9dc18b402cde8f315e7f3e10d202e1e8b06a
Vinh Pham <vinh@vinh.dev>
2024-09-09 06:30:03 +0000

    tweak colors

--- uno.config.ts
+++ uno.config.ts
@@ -12,7 +12,7 @@ import {
 export default defineConfig({
   shortcuts: [
     {
-      'bg-main': 'bg-hex-eef5fc dark:bg-hex-0d1117',
+      'bg-main': 'bg-hex-ffffff dark:bg-hex-050505',
       'text-main': 'text-hex-555555 dark:text-hex-bbbbbb',
       'text-link': 'text-dark dark:text-white',
       'border-main': 'border-truegray-300 dark:border-truegray-600',
