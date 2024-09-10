dcb9663491fce219d1b5011a1943a55c6bc2ebc0
Vinh Pham <vinh@vinh.dev>
2024-09-10 06:58:39 +0000

    refine fix 4

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -112,7 +112,7 @@ onUnmounted(() => {
         >
           Close
         </button> -->
-        <span class="sticky top-0 z-10 flex items-center justify-center gap-1 bg-[#ffffffec] px-6 py-4 text-xs text-zinc-500 backdrop-blur dark:bg-[#0a0910ec] dark:text-zinc-400">
+        <span class="sticky top-0 z-10 flex items-center justify-center gap-1 from-[#ffffffec] to-[#ffffff88] bg-gradient-to-b py-4 text-xs text-zinc-500 backdrop-blur -mx-6 dark:bg-[#0a0910ec] dark:text-zinc-400">
           Press
           <kbd>esc</kbd>
           or click outside to dismiss
