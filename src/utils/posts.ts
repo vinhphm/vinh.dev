9973c1eaf92ce1f7325bd537a5d0f29e049acc0f
Vinh Pham <vinh@vinh.dev>
2024-11-08 07:49:57 +0000

    upgrade to astro 5

--- src/utils/posts.ts
+++ src/utils/posts.ts
@@ -1,12 +1,12 @@
-import { getCollection } from 'astro:content'
 import type { CollectionPosts, PostKey } from '@/types'
+import { getCollection } from 'astro:content'
 
 export function sortPostsByDate(itemA: CollectionPosts, itemB: CollectionPosts) {
   return new Date(itemB.data.date).getTime() - new Date(itemA.data.date).getTime()
 }
 
 export async function getPosts(path?: string, collection: PostKey = 'blog') {
-  return (await getCollection(collection, (post) => {
-    return (import.meta.env.PROD ? post.data.draft !== true : true) && (path ? post.slug.includes(path) : true)
+  return (await getCollection(collection, (post: CollectionPosts) => {
+    return (import.meta.env.PROD ? post.data.draft !== true : true) && (path ? post.id.includes(path) : true)
   })).sort(sortPostsByDate)
 }
