2a2254a83e14982009949187dc49b8d41ec57173
Vinh Pham <vinh@vinh.dev>
2024-11-08 02:45:53 +0000

    refactor

--- uno.config.ts
+++ uno.config.ts
@@ -48,6 +48,36 @@ export default defineConfig({
     }),
   ],
   theme: {
+    colors: {
+      background: 'hsl(var(--background))',
+      foreground: 'hsl(var(--foreground))',
+      primary: {
+        DEFAULT: 'hsl(var(--primary))',
+        foreground: 'hsl(var(--primary-foreground))',
+      },
+      secondary: {
+        DEFAULT: 'hsl(var(--secondary))',
+        foreground: 'hsl(var(--secondary-foreground))',
+      },
+      muted: {
+        DEFAULT: 'hsl(var(--muted))',
+        foreground: 'hsl(var(--muted-foreground))',
+      },
+      accent: {
+        DEFAULT: 'hsl(var(--accent))',
+        foreground: 'hsl(var(--accent-foreground))',
+      },
+      additive: {
+        DEFAULT: 'hsl(var(--additive))',
+        foreground: 'hsl(var(--additive-foreground))',
+      },
+      destructive: {
+        DEFAULT: 'hsl(var(--destructive))',
+        foreground: 'hsl(var(--destructive-foreground))',
+      },
+      border: 'hsl(var(--border))',
+      ring: 'hsl(var(--ring))',
+    },
     extend: {
       maxWidth: {
         '65ch': '65ch',
