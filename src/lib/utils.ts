026c97788e8194e65bcb1f778b409a77cbf0b032
Vinh Pham <vinh@vinh.dev>
2025-01-07 10:18:20 +0000

    use short month

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,7 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 export function formatDate(date: Date) {
   return Intl.DateTimeFormat('en-US', {
     year: 'numeric',
-    month: 'long',
+    month: 'short',
     day: 'numeric',
   }).format(date)
 }
